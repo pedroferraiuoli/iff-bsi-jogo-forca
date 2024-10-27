276642765a5fb9d72ee9c2baca932abfca672eed
Ivanilso da Silva Conceicão Filho <ivanilsodasilva300@gmail.com>
2024-10-26 21:15:53 -0300

    FEAT: Palalvra

--- src/br/edu/iff/bancodepalavras/dominio/palavra/Palavra.java
+++ src/br/edu/iff/bancodepalavras/dominio/palavra/Palavra.java
@@ -1,37 +1,47 @@
 package br.edu.iff.bancodepalavras.dominio.palavra;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.edu.iff.bancodepalavras.dominio.letra.Letra;
 import br.edu.iff.bancodepalavras.dominio.letra.LetraFactory;
 import br.edu.iff.bancodepalavras.dominio.tema.Tema;
 import br.edu.iff.dominio.ObjetoDominioImpl;
 
 public class Palavra extends ObjetoDominioImpl {
+	private static LetraFactory letraFactory = null;
 	private Tema tema;
-	private LetraFactory letraFactory;
 	private Letra[] letras;
-	private boolean[] posicoes;
-	private Object context;
 	
 	private Palavra(long id, String palavra, Tema tema) {
 		super(id);
-		this.tema = tema; 
+		
+		if (letraFactory == null) {
+			throw new IllegalStateException("Atributo letraFactory indefinido");
+		}
 		
 		letras =  new Letra[palavra.length()];
-		for (char string : palavra.toCharArray()) {
-			
+		for (int i = 0; i<palavra.length();i++) {
+			letras[i] = letraFactory.getLetra(palavra.charAt(i));
 		}
+		
+		this.tema = tema; 
 	}
 	
-	public Palavra criar(long id, String palavra, Tema tema) {
+	public static Palavra reconstituir(long id, String palavra, Tema tema) {
 		return new Palavra(id, palavra, tema);
 	}
-
-	public LetraFactory getLetraFactory() {
-		return letraFactory;
+	
+	public static Palavra criar(long id, String palavra, Tema tema) {
+		return new Palavra(id, palavra, tema);
+	}
+	
+	public void setLetraFactory(LetraFactory letraFactory) {
+		Palavra.letraFactory = letraFactory;
 	}
 
-	public void setLetraFactory(LetraFactory letraFactory) {
-		this.letraFactory = letraFactory;
+	public static LetraFactory getLetraFactory() {
+		return letraFactory;
 	}
 	
 	public Tema getTema() {
@@ -39,14 +49,44 @@ public class Palavra extends ObjetoDominioImpl {
 	}
 	
 	public int getTamanho() {
-		return palavra.length();
+		return letras.length;
 	}
 	
 	public Letra[] getLetras() {
 		return null;
 	}
 	
+	public void exibir(boolean[] posicoes) {
+		for (int i = 0; i < posicoes.length; i++) {
+			if (posicoes[i]) {
+				System.out.print(letras[i]);
+			}
+			System.out.println(Palavra.letraFactory.getLetraEncoberta());
+		}
+	}
+	
 	public boolean comparar(String palavra) {
-		return this.palavra == palavra;
+		return this.toString() == palavra;
+	}
+	
+	public int[] tentar(char codigo) {
+		List<Integer> posicoes = new ArrayList<>();
+		
+		for (int i=0;i<letras.length;i++) {
+			if (letras[i].getCodigo() == codigo) {
+				posicoes.add(i);
+			}
+		}
+		
+		return posicoes.stream().mapToInt(Integer::intValue).toArray();
+	}
+	
+	@Override
+	public String toString() {
+		String palavra = "";
+		for (Letra letra : letras) {
+			palavra = palavra + letra;
+		}
+		return palavra;
 	}
 }
---
+++ src/br/edu/iff/teste/dominio/palavra/TestePalavra.java
@@ -0,0 +1,14 @@
+package br.edu.iff.teste.dominio.palavra;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestePalavra {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
